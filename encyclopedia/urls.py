300a1b5eaf1a01d6a4d15a7100ec3a332a1eb1a2
Eduardo Salinas <eduardoluis93@gmail.com>
2020-12-07 16:46:06 +0000

    Updated files
    I was able to redirect the user to the CSS wiki entry if the user goes to '127.0.0.1/wiki', and the CSS page was rendered.

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -7,5 +7,10 @@ urlpatterns = [
 
     # If the user types "127.0.0.1/test", they will see "hello world". See "views.py" for more
     # details on what "test" shows. IT WORKS.
-    path("test", views.test, name="test")
+    path("test", views.test, name="test"),
+
+    # This url will send the user to the wiki entry for the "CSS" article. See the "wiki_entry" function from
+    # the "views.py" file for more details. IT WORKS. The problems I was getting were with the "views.py" file,
+    # NOT with this file.
+    path("wiki", views.wiki_article, name="wiki_article"),
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -8,8 +8,37 @@ def index(request):
         "entries": util.list_entries()
     })
 
-# This will show the page defined on the "test.html" file. I need to add "test"
-# in "urls.py". IT WORKS.
+
+"""
+This will show the page defined on the "test.html" file. I need to add "test"
+in "urls.py". IT WORKS. 
+
+The "encyclopedia" in "encyclopedia/test.html" refers to the folder named "encyclopedia" within the "templates" folder.
+So, if I want to redirect the user to "entries", I would need to create an "entries" folder inside the "templates"
+folder. HOWEVER, since I can just call the entries by using the function "get_entry" from util.py, I don't need
+to redirect the user to "CSS.md" (nor the rest fo the entries.) Just look at the "index" function above me, and I will 
+modify it so that I use the "get_entry" function to go to the "CSS.md" entry.
+"""
+
+
 def test(request):
     return render(request, "encyclopedia/test.html")
+
+
+"""
+This will render the CSS wiki entry. To do that, I will use the "get_entry" function from util.py. It seems that
+it will be displayed on the index.html file, so that may give me an error. 
+
+I need to go to "urls.py" from the "encyclopedia" folder, and add a path to show this (like "wiki"). IT WORKS. 
+I needed to put "CSS" between quotation marks to make it work.
+
+Now, I need to edit it so that it detects the name of ANY of the entries (CSS, Django, Git, etc.) I think that this
+tutorial from freecodecamp may help me: https://www.youtube.com/watch?v=F5mRW0jo-U4&t=11061s .
+"""
+
+
+def wiki_article(request):
+    return render(request, "encyclopedia/index.html", {
+        "entries": util.get_entry("CSS")
+    })
 
