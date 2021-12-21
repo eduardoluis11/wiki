fb10d657a9ab575023cef0cce9f3f678f526067c
Eduardo <eduardoluis93@gmail.com>
2021-12-21 13:09:50 +0000

    Test Commit
    I's updating the pycache file so that I can pull my repo from github into the Desktop computer without git giving me errors.

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -2,6 +2,12 @@ from django.shortcuts import render
 
 from . import util
 
+# This will let me use "markdown", the package that converts Markdown into HTMl, so that the 
+# text from the entry file is properly displayed instead of having the werid spaces, and so 
+# the "#" characters convert the Markdown text into bold or italics when displayed in the web app
+# (source: https://github.com/trentm/python-markdown2). 
+import markdown2
+
 # This will allow me to show a "404: page not found" error.
 # Source: https://docs.djangoproject.com/en/3.1/intro/tutorial03/
 from django.shortcuts import get_object_or_404
@@ -53,11 +59,16 @@ I want to raise a "404" error if "None2 is returned (I will display the message 
 
 I changed the word "entry" by "word" in both the function wiki_article() and on "util.get_entry(word)" so that I know
 that that word is the one being typed by the user (the one obtained by "<str:" in urlpatterns.)
+
+To properly convert from Markdown into HTML so that the entry text doesn't have any weird space between 
+each letter, and so that the "#" characters are able to make the text bold or italices (as it should in Markdown
+format), I will use the "markdown.markdown(ENTRY_TEXT_VARIABLE)" function from the markdown2 package 
+(source: https://github.com/trentm/python-markdown2 .)
 """
 def wiki_article(request, word):
     # I will get the word from "<str:" from urlpatterns from urls.py by assigning a variable and using the
     # util.get_entry() function. This will let me to easily manipulate the word obtained.
-    article = util.get_entry(word)
+    article = markdown2.markdown(util.get_entry(word))
 
     # If I insert the wrong word (that is, a non-existent wiki entry) into "wiki/article_name" while typing on the URL,
     # Django/Python will return "None" (like "Null" in other languages). Therefore, "article" will be "None" if I type
@@ -77,18 +88,25 @@ def wiki_article(request, word):
 
     # If the user types the right article name (be it because it's one of the 6 entries that came by default with
     # this source code, or be it because the user created a new article), this will send the user to that wiki article.
-    return render(request, "encyclopedia/index.html", {
+    #
+    # I created a file called "display_entry.html", which is where I will redirect the user if they type an
+    # existing entry name. There, I have a template (like layout.html), but that I will only use to show each entry.
+    return render(request, "encyclopedia/display_entry.html", {
         # I will use the word "word" instead of "entry" to know that this word is the one being obtained from
         # urlpatterns from "urls.py". IT WORKS PERFECTLY.
 
-        # The word between quotation marks needs to be "entries" or won't work. I will insert here the "article"
-        # variable (the word typed by the user on the URL bar.) This seems to be like sanitizing data in PHP.
-        "entries": article
+        # The word between quotation marks needs to be "entries" or won't work. I think that's because "entries" is the
+        # name of the folder that contains all of the articles as ".md" files.
+        #
+        # I will insert here the "article" variable (the word typed by the user on the URL bar.) This seems to be like
+        # sanitizing data in PHP.
+        "entries": article 
     })
 
-
-
-
+# def get_title(request):
+#     return render(request, "encyclopedia/display_entry.html", {
+#         "entries": util.list_entries()
+#     })
 
 
 
